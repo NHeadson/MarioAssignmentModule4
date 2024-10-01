3c35335afd90695818ffa5a0ab7fb4a78b7222c4
Headsonn <nheadson@my.wctc.edu>
2024-10-01 13:28:24 -0500

    [2.02] Removed parallel lists

--- Program.cs
+++ Program.cs
@@ -11,14 +11,9 @@ if (!File.Exists(file))
 }
 else
 {
-    //create parallel lists of character details
-    List<UInt64> Ids = [];
-    List<string> Names = [];
-    List<string?> Descriptions = [];
-    List<string?> Species = [];
-    List<string?> FirstAppearance = [];
-    List<UInt64?> YearCreated = [];
-    // to populate the lists with data, read from the data file
+    //create list of characters
+    //to populate the list with data, read from the data file
+    List<Character> characters = [];
     try
     {
         StreamReader sr = new(file);
@@ -29,20 +24,22 @@ else
             string? line = sr.ReadLine();
             if (line is not null)
             {
+                Character character = new();
                 // character details are separated with comma(,)
                 string[] characterDetails = line.Split(',');
                 // 1st array element contains id
-                Ids.Add(UInt64.Parse(characterDetails[0]));
+                character.Id = UInt64.Parse(characterDetails[0]);
                 // 2nd array element contains character name
-                Names.Add(characterDetails[1]);
+                character.Name = characterDetails[1];
                 // 3rd array element contains character description
-                Descriptions.Add(characterDetails[2]);
+                character.Description = characterDetails[2];
                 // 4th array element contains character species
-                Species.Add(characterDetails[3]);
+                character.Species = characterDetails[3];
                 // 5th array element contains character first appearance
-                FirstAppearance.Add(characterDetails[4]);
+                character.FirstAppearance = characterDetails[4];
                 // 6th array element contains character year created
-                YearCreated.Add(UInt64.Parse(characterDetails[5]));
+                character.YearCreated = UInt64.Parse(characterDetails[5]);
+                characters.Add(character);
             }
         }
         sr.Close();
@@ -51,7 +48,7 @@ else
     {
         logger.Error(ex.Message);
     }
-
+/*
     string? choice;
     do
     {
@@ -127,6 +124,7 @@ else
             }
         }
     } while (choice == "1" || choice == "2");
+    */
 }
 
 logger.Info("Program ended");
